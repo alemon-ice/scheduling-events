f45e2d90cb72fbea4dae7b65a559ce8280ef82d9
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2020-08-16 17:54:03 -0300

    Validation with Celebrate

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 
 import RoomsController from './controllers/RoomsController';
 import EventsController from './controllers/EventsController';
@@ -9,13 +10,26 @@ const roomsController = new RoomsController();
 const eventsController = new EventsController();
 
 routes.get('/rooms', roomsController.index);
-routes.post('/rooms', roomsController.create);
+routes.post('/rooms', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().max(50),
+    building: Joi.string().required().max(50),
+  }),
+}, { abortEarly: false }),
+  roomsController.create);
 routes.put('/rooms/:id', roomsController.update);
 routes.delete('/rooms/:id', roomsController.delete);
 
 routes.get('/events', eventsController.index);
 routes.get('/events_day', eventsController.eventsOfDay);
-routes.post('/events', eventsController.create);
+routes.post('/events', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().max(50),
+    description: Joi.string().required().max(200),
+    responsible: Joi.string().required().max(50),
+  })
+}),
+  eventsController.create);
 routes.put('/events/:id', eventsController.update);
 routes.delete('/events/:id', eventsController.delete);
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -9,5 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.listen(3333);
